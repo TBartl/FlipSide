8d40410ea38bf5b696dec0c26967129a00f844cd
TBartl <tbartl@umich.edu>
2016-01-16 16:58:26 -0500

    Added some levels

---
+++ 48PajamaJam/Assets/Portal.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+    
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.name == "Player")
+            GameManager.instance.GotoNextLevel();
+    }
+}
