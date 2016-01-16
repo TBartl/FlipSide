1e6734cdfa1dea26a865d7cf8fe623e1a8834d38
Ben Willshire <bwillsh@umich.edu>
2016-01-16 17:51:36 -0500

    key flaots

--- 48PajamaJam/Assets/Scripts/Key.cs
+++ 48PajamaJam/Assets/Scripts/Key.cs
@@ -6,14 +6,24 @@ public class Key : MonoBehaviour {
     bool hit = false;
     public float targetScale = .001f;
     public float shrinkSpeed = 5f;
+    public float rotateRate = .75f;
+
+    public float init = 4.75f;
+    public float range = .25f / 2;
+    public float TimeToBob = 1f;
     // Use this for initialization
+    float time = 0;
     void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        time += Time.deltaTime;
+        Vector3 pos = transform.position;
+        pos.y = init + Mathf.Sin(2 * Mathf.PI * Time.timeSinceLevelLoad) * range;
+        gameObject.transform.position = pos;
+        gameObject.transform.Rotate(0, rotateRate, 0);
         if(hit)
             transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(targetScale, targetScale, targetScale), Time.deltaTime* shrinkSpeed);
 }
