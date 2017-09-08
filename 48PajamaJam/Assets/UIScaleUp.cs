687ac0c93df717335ac6980752d8330e9868fc87
TBartl <tbartl@umich.edu>
2017-09-08 15:17:15 -0400

    UI Scaling, fixed dialogue, upped volume

---
+++ 48PajamaJam/Assets/UIScaleUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScaleUp : MonoBehaviour {
+    GUIText guiText;
+    int initialFontSize;
+
+    void Awake() {
+        guiText = this.GetComponent<GUIText>();
+        initialFontSize = guiText.fontSize;
+    }
+
+
+    // Use this for initialization
+    void Update () {
+        guiText.fontSize = initialFontSize * Screen.width / 1280;		
+	}
+}
